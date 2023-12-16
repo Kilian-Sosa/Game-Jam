c4f9dc8446e6b51cbf182285b8e5a6c91fef4aa6
Kilian-Sosa <kilianjosesosaguillen@alumno.ieselrincon.es>
2023-12-16 13:14:25 +0000

    refactor: Change Enemy Behaviour

---
+++ Assets/Scripts/Enemy2Controller.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class Enemy2Controller : MonoBehaviour {
+    [SerializeField] float moveSpeed = 5f;
+    [SerializeField] float moveDistance = 3f;
+
+    private Vector2 initialPosition;
+    private bool direction = true;
+
+    void Start() {
+        initialPosition = transform.position;
+    }
+
+    void Update() {
+        RaycastHit2D hit = Physics2D.Raycast(
+            new Vector2(transform.position.x, transform.position.y + 1), new Vector2(0, 1f), 1f);
+        if (direction) {
+            transform.Translate(Vector2.up * moveSpeed * Time.deltaTime);
+
+            if (transform.position.y >= initialPosition.y + moveDistance)
+                direction = false;
+        } else {
+            hit = Physics2D.Raycast(
+            new Vector2(transform.position.x, transform.position.y - 1), new Vector2(0, -1f), 1f);
+            transform.Translate(Vector2.down * moveSpeed * Time.deltaTime);
+
+            if (transform.position.y <= initialPosition.y - moveDistance)
+                direction = true;
+        }
+
+        if (hit.collider != null) direction = !direction;
+    }
+}
