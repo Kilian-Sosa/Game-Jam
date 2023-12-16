1c7f6a47cdf6bede27cc5be7358e91cd035e164a
AncorRS <ancorrs@hotmail.com>
2023-12-16 15:35:18 +0100

    mejoras y salto Ancor

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,26 +6,27 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody2D rb;
-    private Animator anim;
     public Vector2 direccion;
 
     [Header("Estadisticas")]
     public float velocidaDeMovimiento = 10;
     public float fuerzaDeSalto = 5;
 
-    [Header("Colisiones")]
-    public LayerMask layerPiso;
-    public float radioDeColision;
-    public Vector2 abajo;
-
     [Header("Booleanos")]
     public bool puedeMover = true;
-    public bool enSuelo = true; //para controlar el salto en el aire
+    public bool enSuelo = true;
+
+    public bool primerSalto = true;
+    public bool salto = true;
+
+    public int contadorSalto = 0;
+
+    [SerializeField] private GameObject AttackTriggerBox;
+
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
-        anim = GetComponent<Animator>();
     }
 
     // Start is called before the first frame update
@@ -37,38 +39,48 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         Movimiento();
-        Agarres();
 
-        if (Input.GetKeyDown("space"))
+        if (primerSalto)
         {
-           
-
-            GetComponent<Rigidbody2D>().AddForce(new Vector3(0, 300f, 0));
-            Debug.Log("Tecla espacio");
-            //GameObject.Find("SoundManager").GetComponent<soundManager>().PlayAudio("saltar");
-            //GameObject.Find("Plataforma").GetComponent<TilemapCollider2D>().
+            Saltar();
         }
+
+ 
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            contadorSalto = 0;
+            primerSalto = true;
+        }
+
+        if (collision.gameObject.CompareTag("Enemigorl"))
+        {
+            AplicarDmg(collision.gameObject);
+        }
+    }
+
+
+    private void AplicarDmg(GameObject jugador)
+    {
+        Debug.Log("DAMAGE");
     }
 
     public void Movimiento()
     {
-        float x = Input.GetAxis("Horizontal"); //da como resultado 1 o -1 segun la direccion que vayas
+        float x = Input.GetAxis("Horizontal");
         float y = Input.GetAxis("Vertical");
 
-        //Vector2 direccion = new Vector2(x, y); //te da la direccion del muñeco!!!
         direccion = new Vector2(x, y);
 
-        //Caminar(direccion);
         Caminar();
-        //MejorarSalto();
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            //if (enSuelo)
-            //{
-                //anim.SetBool("saltar", true); //modificamos el booleano saltar para las animaciones 13
+
                 Saltar();
-            //}
         }
 
         if (enSuelo)
@@ -78,47 +90,23 @@ public class PlayerController : MonoBehaviour
                 velocidad = 1;
             else
                 velocidad = -1;
-            //anim.SetBool("caer", false); //13 lo quitamos en el 14
-            anim.SetFloat("velocidadVertical", velocidad);
-        }
-        //else
-        //{
-        //    anim.SetBool("saltar", true); //13 lo sustutiumos por un evento
-        //}
-    }
-
-    public void finalizarSalto() //13
-    {
-        anim.SetBool("saltar", false);
-        anim.SetBool("caer", true);
-    }
-
-    private void MejorarSalto()
-    {
-        // Si la velocidad es menor que cero estoy cayendo
-        if (rb.velocity.y < 0)
-        {
-            rb.velocity += Vector2.up * Physics2D.gravity.y * (5f - 1) * Time.deltaTime;
-        }
-        else if ((rb.velocity.y > 0) && !Input.GetKey(KeyCode.Space))
-        {
-            rb.velocity += Vector2.up * Physics2D.gravity.y * (5f - 1) * Time.deltaTime;
         }
     }
 
-    public void Agarres() //para controlar el salto doble
-    {
-        //hacemos casting porque transform es un vector de 3 vectores
-        //el tercer parametro para el suelo
-        enSuelo = Physics2D.OverlapCircle((Vector2)transform.position + abajo, radioDeColision, layerPiso);
-    }
 
     public void Saltar()
     {
-        rb.velocity = new Vector2(rb.velocity.x, 0);
-        //rb.velocity += direccion * fuerzaDeSalto;
-        rb.velocity += Vector2.up * fuerzaDeSalto;
+        if (Input.GetKeyDown("space") && contadorSalto < 2)
+        {
+            salto = true;
+            primerSalto = false;
+
+            GetComponent<Rigidbody2D>().AddForce(new Vector3(0, 15f, 0), ForceMode2D.Impulse);
+            Debug.Log("Tecla espacio");
+            contadorSalto++;
+        }
     }
+
 
     public void Caminar()
     {
@@ -128,15 +116,6 @@ public class PlayerController : MonoBehaviour
 
             if (direccion != Vector2.zero)
             {
-                if (!enSuelo)
-                {
-                    anim.SetBool("caer", true); // si no esta en suelo hago animacion caer, para animaciones 13
-                }
-                else
-                {
-                    anim.SetBool("caminar", true); //13
-                }
-
                 if (direccion.x < 0 && transform.localScale.x > 0)
                 {
                     transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
@@ -147,11 +126,6 @@ public class PlayerController : MonoBehaviour
                     transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
                 }
             }
-            else
-            {
-                anim.SetBool("caminar", false); //13
-            }
-
         }
     }
 
