2e0066d097b09b7980d204b355786fe9f9e6a21d
Kilian-Sosa <kilianjosesosaguillen@alumno.ieselrincon.es>
2023-12-16 16:55:26 +0000

    fix: Small MainTitle fix

---
+++ Assets/Scripts/PlayGame.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayGame : MonoBehaviour
+{
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return)) SCManager.instance.LoadScene("GameScene");
+    }
+}
