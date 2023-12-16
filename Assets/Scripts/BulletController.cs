f48db9abc49374d287c80366b1ea2334fe4b3126
Kilian-Sosa <kilianjosesosaguillen@alumno.ieselrincon.es>
2023-12-16 11:59:00 +0000

    Squashed commit of the following:
    commit 4135c863f0ce36863fdf0a023b7f1ef96e156394
    Author: Kilian-Sosa <kilianjosesosaguillen@alumno.ieselrincon.es>
    Date:   Sat Dec 16 11:48:22 2023 +0000
    
        feat: Now the 3rd Enemy only shoots when player on range
    
    commit fd7e1a574b47c340c821258d776d746e5ae0d2b2
    Author: Kilian-Sosa <kilianjosesosaguillen@alumno.ieselrincon.es>
    Date:   Sat Dec 16 11:34:53 2023 +0000
    
        feat: Add 3rd Enemy and attack (bullet))
    
    commit 7cee7f19a670c01baa54882fad7519bf2670fd8f
    Author: Kilian-Sosa <kilianjosesosaguillen@alumno.ieselrincon.es>
    Date:   Sat Dec 16 10:22:35 2023 +0000
    
        fix: Now both anims are working successfully
    
    commit 9cd6e148830007e86b4e0c0b54a7ec1c8d8f13c2
    Author: Kilian-Sosa <kilianjosesosaguillen@alumno.ieselrincon.es>
    Date:   Sat Dec 16 09:51:20 2023 +0000
    
        feat: Add 2nd Enemy & nim
    
    commit 8c1c12d22786d38b1cc17312bba5d092e054ef76
    Author: Kilian-Sosa <kilianjosesosaguillen@alumno.ieselrincon.es>
    Date:   Sat Dec 16 09:42:16 2023 +0000
    
        feat: Add 1st Enemy & anim

---
+++ Assets/Scripts/BulletController.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BulletController : MonoBehaviour {
+    [SerializeField] float bulletSpeed = 20f;
+
+    void Start() {
+        Vector2 difference = GameObject.Find("Player").transform.position - transform.position;
+        GetComponent<Rigidbody2D>().velocity = difference.normalized * bulletSpeed;
+        Destroy(gameObject, 1.5f);
+    }
+
+    void Update() {
+        if (transform.position.x < GameObject.Find("Player").GetComponent<Transform>().position.x) {
+            //hud.score++;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.gameObject.tag == "Player") {
+            //GameObject.Find("SoundManager").GetComponent<soundManager>().PlayAudio("collision");
+            //SceneManager.LoadScene("Lose", LoadSceneMode.Single);
+        }
+    }
+}
---
+++ Assets/Scripts/Enemy3Controller.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour {
+
+    [SerializeField] GameObject bulletEnemyPrefab;
+    private GameObject bullet = null;
+
+    void Update() {
+        Vector2 difference = GameObject.Find("Player").transform.position - transform.position;
+        if (difference.x > 0) return;
+        RaycastHit2D hit = Physics2D.Raycast(
+            new Vector2(transform.position.x -2, transform.position.y), difference.normalized, 10f);
+
+        Debug.DrawRay(new Vector2(transform.position.x - 1,
+            transform.position.y), difference.normalized * 10f, Color.red);
+
+        if (hit.collider != null && hit.collider.name == "Player") Shoot();        
+    }
+
+    private void Shoot() {
+        if (bullet != null) return;
+
+        bullet = Instantiate(bulletEnemyPrefab, transform.position, Quaternion.identity);
+        //GameObject.Find("SoundManager").GetComponent<SoundManager>().PlayAudio("enemyShot");
+        new WaitForSeconds(2.5f);
+    }
+}
